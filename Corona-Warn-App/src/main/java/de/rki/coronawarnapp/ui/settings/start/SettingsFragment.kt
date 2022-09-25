dc39caeff363c2901c7206b1e2d6a3631fd09c64
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-09-25 10:17:10 +0200

    Per-App language settings

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/start/SettingsFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/start/SettingsFragment.kt
@@ -3,8 +3,13 @@ package de.rki.coronawarnapp.ui.settings.start
 import android.os.Bundle
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
+import androidx.appcompat.app.AppCompatDelegate.getApplicationLocales
+import androidx.appcompat.app.AppCompatDelegate.setApplicationLocales
+import androidx.core.os.LocaleListCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import de.rki.coronawarnapp.BuildConfig
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentSettingsBinding
 import de.rki.coronawarnapp.ui.doNavigate
@@ -14,6 +19,7 @@ import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import java.util.Locale
 import javax.inject.Inject
 
 /**
@@ -42,6 +48,22 @@ class SettingsFragment : Fragment(R.layout.fragment_settings), AutoInject {
         vm.analyticsState.observe2(this) {
             binding.analyticsState = it
         }
+
+        val languages = BuildConfig.SUPPORTED_LOCALES.map { it.displayName }.toTypedArray()
+        binding.settingsAppLanguage.root.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.settings_app_language)
+                .setCancelable(false)
+                .setSingleChoiceItems(
+                    languages,
+                    BuildConfig.SUPPORTED_LOCALES.indexOf(getApplicationLocales().toLanguageTags())
+                ) { _, which ->
+                    setApplicationLocales(LocaleListCompat.forLanguageTags(BuildConfig.SUPPORTED_LOCALES[which]))
+                    binding.settingsAppLanguage.statusText = getApplicationLocales().toLanguageTags().displayName
+                }.show()
+        }
+
+        binding.settingsAppLanguage.statusText = getApplicationLocales().toLanguageTags().displayName
 
         setButtonOnClickListener()
     }
@@ -87,4 +109,6 @@ class SettingsFragment : Fragment(R.layout.fragment_settings), AutoInject {
 
         binding.toolbar.setNavigationOnClickListener { popBackStack() }
     }
+
+    private val String.displayName get() = Locale(this).let { it.getDisplayLanguage(it) }
 }
